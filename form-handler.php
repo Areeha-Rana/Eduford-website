a274c45a55b8bca06ceb44f14bf5005b439e4207
Areeha-Rana <areeharana2288@gmail.com>
2025-12-22 19:18:31 +0500

    Update form-handler.php

--- form-handler.php
+++ form-handler.php
@@ -1,24 +1,25 @@
-<?php 
+<?php
+$name = $_POST['contact-name'];
+$visitor_email = $_POST['contact-email'];
+$subject = $_POST['contact-subject'];
+$message = $_POST['contact-message'];
 
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+$email_from = 'info@YOURDOMAIN';
 
-    $email_from = 'areeharana2288@gmail.com';
-    $email_subject = 'New Form Submission';
-    $email_body = "User Name: $name.\n".
-                    "User Email: $visitor_email.\n".
-                    "Subject: $subject.\n".
-                     "User Message: $message .\n";   
+$email_subject = 'New Form Submission';
 
-$to = 'areeharana2288@gmail.com';
+$email_body = "User Name: $name.\n".
+              "User Email: $visitor_email.\n".
+              "Subject: $subject. \n".
+              "User Message: $message .\n";
+
+$to = 'mitsosxou@yahoo.gr';
+
 $headers = "From: $email_from \r\n";
-$headers .="Reply-To: $visitor_email \r\n";
+
+$headers .= "Reply-To: $visitor_email \r\n";
 
 mail($to,$email_subject,$email_body,$headers);
 
-header("Location: contact.html");
-
-
+header("Location: contact.html")
 ?>
